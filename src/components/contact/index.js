30a38e55600acf931ebcc20477a24d1cc25787ef
WeichunAuto <seven.wangweichun@outlook.com>
2024-10-17 20:02:32 +1300

    validate the form fields from the front end side.

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,10 +1,80 @@
 import React, { Component } from 'react'
 import { Button, Input, Textarea, Checkbox } from '@nextui-org/react'
-import { HeartIcon } from '../body/icons'
+import { HeartIcon, MailIcon, PhoneIcon } from '../body/icons'
 import SocialMedia from '../aboutMe/socialMedia'
+import { capitalize } from '../utils'
 
 export class Contact extends Component {
+
+  state = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    message: '',
+    isSubscribe: false,
+
+    isFirstNameInvalid: false,
+    isLastNameInvalid: false,
+    isEmailInvalid: false,
+    isPhoneNumberInvalid: false,
+    isMessageInvalid: false
+  }
+
+  handleInput = (e) => {
+    const {name, value} = e.target
+    if(name === 'isSubscribe') {
+      this.setState({[name]: e.target.checked})
+    } else {
+      this.setState({[name]: value})
+      this.setState({['is'+capitalize(name)+'Invalid']: false})
+    }
+  }
+
+  handleSubmit = () => {
+    if (!this.validateFileds()) return false
+    
+  }
+
+  validateFileds = () => {
+    const {firstName, lastName, email, phoneNumber, message} = this.state
+    if(firstName.trim() === '') {
+      this.setState({isFirstNameInvalid: true})
+      return false
+    }
+    if(lastName.trim() === '') {
+      this.setState({isLastNameInvalid: true})
+      return false
+    }
+
+    const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+    const emailInvalid = (value) => {
+      if(value.trim() === '') return true
+      return validateEmail(value) ? false : true
+    }
+    if(emailInvalid(email)) {
+      this.setState({isEmailInvalid: true})
+      return false
+    }
+
+    const validatePhone = (value) => value.match(/^\+|0\d+$/)
+    const phoneInvalid = (value) => {
+      if(value.trim() === '') return true
+      return validatePhone(value) ? false : true
+    }
+    if(phoneInvalid(phoneNumber)) {
+      this.setState({isPhoneNumberInvalid: true})
+      return false
+    }
+
+    if(message.trim() === '') {
+      this.setState({isMessageInvalid: true})
+      return false
+    }
+  }
+
   render() {
+    const {firstName, lastName, email, phoneNumber, message, isSubscribe, isFirstNameInvalid, isLastNameInvalid, isEmailInvalid, isPhoneNumberInvalid, isMessageInvalid} = this.state
     return (
       <div className='w-full pt-16 h-auto'>
         <p className='font-georgian text-5xl lg:text-7xl text-center mb-8'>get in touch.</p>
@@ -16,27 +86,63 @@ export class Contact extends Component {
               <div className='flex flex-col lg:flex-row gap-6 lg:gap-8'>
                 <Input type='text' label='First Name' placeholder="Enter your first name" className='border-none'
                   isRequired
-                  isInvalid={true}
-                  errorMessage = 'Please provide your email address.'
+                  name='firstName'
+                  value={firstName}
+                  isInvalid={isFirstNameInvalid}
+                  errorMessage = 'Please provide your first name.'
+                  onChange={this.handleInput}
                 />
                 <Input type='text' label='Last Name' placeholder="Enter your last name"
                   isRequired
+                  name='lastName'
+                  value={lastName}
+                  isInvalid={isLastNameInvalid}
+                  errorMessage = 'Please provide your last name.'
+                  onChange={this.handleInput}
                 />
               </div>
               <div className='flex flex-col lg:flex-row gap-6 lg:gap-8 mt-8'>
                 <Input type='email' label='Email'  placeholder="Enter your email" className='border-none' 
                   isRequired
-                  isInvalid={true}
+                  name='email'
+                  value={email}
+                  isInvalid={isEmailInvalid}
                   errorMessage = 'Please provide your email address.'
+                  onChange={this.handleInput}
+                  startContent={
+                    <MailIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+                  }
                 />
-                <Input type='text' label='Phone Number (Optional)'   placeholder="Enter your phone number" />
+                <Input type='text' label='Phone Number (Optional)' placeholder="Enter your phone number" 
+                  name='phoneNumber'
+                  value={phoneNumber}
+                  isInvalid={isPhoneNumberInvalid}
+                  errorMessage = 'Please provide a correct phone number format.'
+                  onChange={this.handleInput}
+                  startContent={
+                    <PhoneIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                />
               </div>
 
               <div className='mt-8'>
-                <Textarea label='Message' placeholder='What do you have in your mind?'/>
+                <Textarea label='Message' placeholder='What do you have in your mind?'
+                  name='message'
+                  isInvalid={isMessageInvalid}
+                  errorMessage = 'Please write what you want to say.'
+                  value={message}
+                  onChange={this.handleInput}
+                />
               </div>
               <div className='mt-2'>
-                <Checkbox size='sm'>Subscribe me to receive my latestwork.</Checkbox>
+                <Checkbox size='sm' defaultSelected={isSubscribe}
+                  name='isSubscribe'
+                  value={isSubscribe}
+                  isSelected={isSubscribe}
+                  onChange={this.handleInput}
+                >
+                  Subscribe me to receive my latestwork.
+                </Checkbox>
               </div>
 
               <div className='mt-10 w-full flex justify-end'>
@@ -44,6 +150,7 @@ export class Contact extends Component {
                   startContent={<HeartIcon width='20px' height='20px'/>}
                   isLoading={false}
                   className='bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg'
+                  onPress={this.handleSubmit}
                 >
                   SEND
                 </Button>
